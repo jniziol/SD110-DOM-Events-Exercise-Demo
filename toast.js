967b1e1aabd036ea5d5ff14594145cf8fe7e5526
Johnathan Niziol <35515+jniziol@users.noreply.github.com>
2020-12-02 11:13:32 -0600

    Add files via upload

--- toast.js
+++ toast.js
@@ -2,10 +2,14 @@ const toast = document.getElementById('toast');
 
 toast.addEventListener('click', function(e) {  
   if (e.target.nodeName === "I") {
-    toast.classList.toggle('hidden');
+    toast.classList.add('hidden');
   }
 });
 
 setTimeout(function() {
-  toast.classList.toggle('hidden');
+  toast.classList.remove('hidden');
+
+  setTimeout(function() {    
+    toast.classList.add('hidden');
+  }, 5000);
 }, 2000);
